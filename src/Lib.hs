53316d58e2aa61383234e5ce4e8219c737bb8022
Manuel Leduc <manuel.leduc@inria.fr>
2016-05-03 18:10:11 +0200

    auth client ok

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,7 @@ module Lib
     ( someFunc
     ) where
 
+import Registry.Client (auth)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
---
+++ src/Registry/Client.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Registry.Client (auth, AuthResponseBody(..)) where
+
+import GHC.Generics
+import Data.Aeson
+import qualified Network.Wreq as R
+import Control.Lens ((^?))
+import Control.Exception as E
+import Network.HTTP.Client (HttpException(StatusCodeException))
+
+data Auth = Auth {
+    username :: String,
+    password :: String,
+    rememberMe :: Bool
+} deriving (Generic)
+
+data AuthResponseBody = AuthResponseBody {
+    id_token :: String
+} deriving (Generic, Show)
+
+instance ToJSON Auth
+instance FromJSON AuthResponseBody
+
+baseUrlServer = "http://localhost:8080/"
+authenticateResource = baseUrlServer ++ "api/authenticate"
+
+--auth :: String -> String -> IO (Maybe AuthResponseBody)
+auth login password =
+    let handler e@(StatusCodeException _ _ _) = return Nothing
+        authObj = Auth login password False
+        operations = do r <- R.post authenticateResource (toJSON authObj)
+                        (body :: R.Response AuthResponseBody) <- R.asJSON r
+                        return (body ^? R.responseBody)
+    in operations `E.catch` handler
